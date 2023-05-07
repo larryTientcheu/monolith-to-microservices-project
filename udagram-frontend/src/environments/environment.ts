946e93a8752d85100309ed878985f9b907b22e0a
larry <larrytientcheu1@gmail.com>
2023-05-07 18:54:09 +0200

    frontend failing

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://a4b01f8a37b74475b9f333efd43aa5fc-1462914454.us-east-1.elb.amazonaws.com:8080/api/v0'
+  apiHost: 'http://localhost:8080/api/v0'
 };
 
 /*
